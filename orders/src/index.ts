e4dd8f714d99b8a8c982a1cac165588a1a278f04
Risal Walangadi <risal.walangadi@gmail.com>
2021-03-02 14:32:25 +0800

    add startup message to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -10,6 +10,8 @@ import {
 import { app } from './app';
 
 const start = async () => {
+  console.log('Starting...');
+  
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
